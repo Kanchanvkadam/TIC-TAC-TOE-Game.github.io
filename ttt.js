84cf00949009ff3a30c0837a1c2851bb9105b829
Kanchanvkadam <kanchanvkadam@gmail.com>
2022-02-12 13:20:56 +0530

    updated

--- ttt.js
+++ ttt.js
@@ -29,19 +29,20 @@ function myfunc() {
     if ((b1 == 'x' || b1 == 'X') && (b2 == 'x' ||
 		b2 == 'X') && (b3 == 'x' || b3 == 'X')) {
 		document.getElementById('print')
-			.innerHTML =`${playerName.first} Won the previous game `;
+			.innerHTML =`Congratulatons !!!  ${playerName.first} Won the Game `;
 		document.getElementById("b4").disabled = true;
 		document.getElementById("b5").disabled = true;
 		document.getElementById("b6").disabled = true;
 		document.getElementById("b7").disabled = true;
 		document.getElementById("b8").disabled = true;
 		document.getElementById("b9").disabled = true;
-		window.alert( `Congratulations! ${playerName.first} wins`);
+		celebration();
+		//window.alert( `Congratulations! ${playerName.first} wins`);
 	}
 	else if ((b1 == 'x' || b1 == 'X') && (b4 == 'x' ||
 		b4 == 'X') && (b7 == 'x' || b7 == 'X')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.first} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.first} Won the Game `;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b3").disabled = true;
 		document.getElementById("b5").disabled = true;
@@ -49,79 +50,79 @@ function myfunc() {
 		document.getElementById("b8").disabled = true;
 		document.getElementById("b9").disabled = true;
 
-		window.alert(`Congratulations! ${playerName.first} wins`);
+		celebration();
 	}
 	else if ((b7 == 'x' || b7 == 'X') && (b8 == 'x' ||
 		b8 == 'X') && (b9 == 'x' || b9 == 'X')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.first} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.first} Won the Game `;
 		document.getElementById("b1").disabled = true;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b3").disabled = true;
 		document.getElementById("b4").disabled = true;
 		document.getElementById("b5").disabled = true;
 		document.getElementById("b6").disabled = true;
-		window.alert(`Congratulations! ${playerName.first} wins`);
+		celebration();
 	}
 	else if ((b3 == 'x' || b3 == 'X') && (b6 == 'x' ||
 		b6 == 'X') && (b9 == 'x' || b9 == 'X')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.first} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.first} Won the Game `;
 		document.getElementById("b1").disabled = true;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b4").disabled = true;
 		document.getElementById("b5").disabled = true;
 		document.getElementById("b7").disabled = true;
 		document.getElementById("b8").disabled = true;
-		window.alert(`Congratulations! ${playerName.first} wins`);
+		celebration();
 	}
 	else if ((b1 == 'x' || b1 == 'X') && (b5 == 'x' ||
 		b5 == 'X') && (b9 == 'x' || b9 == 'X')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.first} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.first} Won the Game `;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b3").disabled = true;
 		document.getElementById("b4").disabled = true;
 		document.getElementById("b6").disabled = true;
 		document.getElementById("b7").disabled = true;
 		document.getElementById("b8").disabled = true;
-		window.alert(`Congratulations! ${playerName.first} wins`);
+		celebration();
 	}
 	else if ((b3 == 'x' || b3 == 'X') && (b5 == 'x' ||
 		b5 == 'X') && (b7 == 'x' || b7 == 'X')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.first} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.first} Won the Game `;
 		document.getElementById("b1").disabled = true;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b4").disabled = true;
 		document.getElementById("b6").disabled = true;
 		document.getElementById("b8").disabled = true;
 		document.getElementById("b9").disabled = true;
-		window.alert(`Congratulations! ${playerName.first} wins`);
+		celebration();
 	}
 	else if ((b2 == 'x' || b2 == 'X') && (b5 == 'x' ||
 		b5 == 'X') && (b8 == 'x' || b8 == 'X')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.first} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.first} Won the Game `;
 		document.getElementById("b1").disabled = true;
 		document.getElementById("b3").disabled = true;
 		document.getElementById("b4").disabled = true;
 		document.getElementById("b6").disabled = true;
 		document.getElementById("b7").disabled = true;
 		document.getElementById("b9").disabled = true;
-		window.alert(`Congratulations! ${playerName.first} wins`);
+		celebration();
 	}
 	else if ((b4 == 'x' || b4 == 'X') && (b5 == 'x' ||
 		b5 == 'X') && (b6 == 'x' || b6 == 'X')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.first} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.first} Won the Game `;
 		document.getElementById("b1").disabled = true;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b3").disabled = true;
 		document.getElementById("b7").disabled = true;
 		document.getElementById("b8").disabled = true;
 		document.getElementById("b9").disabled = true;
-		window.alert(`Congratulations! ${playerName.first} wins`);
+		celebration();
 	}
 
 
@@ -136,91 +137,99 @@ function myfunc() {
 		document.getElementById("b7").disabled = true;
 		document.getElementById("b8").disabled = true;
 		document.getElementById("b9").disabled = true;
-		window.alert(`Congratulations! ${playerName.second} wins`);
+		celebration();
+		//window.alert(`Congratulations! ${playerName.second} wins`);
 	}
 	else if ((b1 == '0' || b1 == '0') && (b4 == '0' ||
 		b4 == '0') && (b7 == '0' || b7 == '0')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.second} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.second} Won the Game `;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b3").disabled = true;
 		document.getElementById("b5").disabled = true;
 		document.getElementById("b6").disabled = true;
 		document.getElementById("b8").disabled = true;
 		document.getElementById("b9").disabled = true;
-		window.alert(`Congratulations! ${playerName.second} wins`);
+		celebration();
+		//window.alert(`Congratulations! ${playerName.second} wins`);
 	}
 	else if ((b7 == '0' || b7 == '0') && (b8 == '0' ||
 		b8 == '0') && (b9 == '0' || b9 == '0')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.second} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.second} Won the Game `;
 		document.getElementById("b1").disabled = true;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b3").disabled = true;
 		document.getElementById("b4").disabled = true;
 		document.getElementById("b5").disabled = true;
 		document.getElementById("b6").disabled = true;
-		window.alert(`Congratulations! ${playerName.second} wins`);
+		celebration();
+		//window.alert(`Congratulations! ${playerName.second} wins`);
 	}
 	else if ((b3 == '0' || b3 == '0') && (b6 == '0' ||
 		b6 == '0') && (b9 == '0' || b9 == '0')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.second} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.second} Won the Game `;
 		document.getElementById("b1").disabled = true;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b4").disabled = true;
 		document.getElementById("b5").disabled = true;
 		document.getElementById("b7").disabled = true;
 		document.getElementById("b8").disabled = true;
-		window.alert(`Congratulations! ${playerName.second} wins`);
+		celebration();
+		//window.alert(`Congratulations! ${playerName.second} wins`);
 	}
 	else if ((b1 == '0' || b1 == '0') && (b5 == '0' ||
 		b5 == '0') && (b9 == '0' || b9 == '0')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.second} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.second} Won the Game `;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b3").disabled = true;
 		document.getElementById("b4").disabled = true;
 		document.getElementById("b6").disabled = true;
 		document.getElementById("b7").disabled = true;
 		document.getElementById("b8").disabled = true;
-		window.alert(`Congratulations! ${playerName.second} wins`);
+		celebration();
+		//window.alert(`Congratulations! ${playerName.second} wins`);
 	}
 	else if ((b3 == '0' || b3 == '0') && (b5 == '0' ||
 		b5 == '0') && (b7 == '0' || b7 == '0')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.second} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.second} Won the Game `;
 		document.getElementById("b1").disabled = true;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b4").disabled = true;
 		document.getElementById("b6").disabled = true;
 		document.getElementById("b8").disabled = true;
 		document.getElementById("b9").disabled = true;
-		window.alert(`Congratulations! ${playerName.second} wins`);
+		celebration();
+		//window.alert(`Congratulations! ${playerName.second} wins`);
 	}
 	else if ((b2 == '0' || b2 == '0') && (b5 == '0' ||
 		b5 == '0') && (b8 == '0' || b8 == '0')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.second} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.second} Won the Game `;
 		document.getElementById("b1").disabled = true;
 		document.getElementById("b3").disabled = true;
 		document.getElementById("b4").disabled = true;
 		document.getElementById("b6").disabled = true;
 		document.getElementById("b7").disabled = true;
 		document.getElementById("b9").disabled = true;
-		window.alert(`Congratulations! ${playerName.second} wins`);
+		celebration();
+		//window.alert(`Congratulations! ${playerName.second} wins`);
 	}
 	else if ((b4 == '0' || b4 == '0') && (b5 == '0' ||
 		b5 == '0') && (b6 == '0' || b6 == '0')) {
 		document.getElementById('print')
-			.innerHTML = `${playerName.second} Won the previous game `;
+			.innerHTML = `Congratulatons !!!  ${playerName.second} Won the Game `;
 		document.getElementById("b1").disabled = true;
 		document.getElementById("b2").disabled = true;
 		document.getElementById("b3").disabled = true;
 		document.getElementById("b7").disabled = true;
 		document.getElementById("b8").disabled = true;
 		document.getElementById("b9").disabled = true;
-		window.alert(`Congratulations! ${playerName.second} wins`);
+		celebration();
+		//window.alert(`Congratulations! ${playerName.second} wins`);
 	}
 
 	else if ((b1 == 'X' || b1 == '0') && (b2 == 'X'
@@ -378,4 +387,34 @@ function myfunc_11() {
 	}
 }
 
-
+function celebration (){
+	for(i=0; i<100; i++) {
+		// Random rotation
+		var randomRotation = Math.floor(Math.random() * 360);
+		  // Random Scale
+		var randomScale = Math.random() * 1;
+		// Random width & height between 0 and viewport
+		var randomWidth = Math.floor(Math.random() * Math.max(document.documentElement.clientWidth, window.innerWidth || 0));
+		var randomHeight =  Math.floor(Math.random() * Math.max(document.documentElement.clientHeight, window.innerHeight || 500));
+		
+		// Random animation-delay
+		var randomAnimationDelay = Math.floor(Math.random() * 15);
+		console.log(randomAnimationDelay);
+	  
+		// Random colors
+		var colors = ['#0CD977', '#FF1C1C', '#FF93DE', '#5767ED', '#FFC61C', '#8497B0']
+		var randomColor = colors[Math.floor(Math.random() * colors.length)];
+	  
+		// Create confetti piece
+		var confetti = document.createElement('div');
+		confetti.className = 'confetti';
+		confetti.style.top=randomHeight + 'px';
+		confetti.style.right=randomWidth + 'px';
+		confetti.style.backgroundColor=randomColor;
+		// confetti.style.transform='scale(' + randomScale + ')';
+		confetti.style.obacity=randomScale;
+		confetti.style.transform='skew(15deg) rotate(' + randomRotation + 'deg)';
+		confetti.style.animationDelay=randomAnimationDelay + 's';
+		document.getElementById("confetti-wrapper").appendChild(confetti);
+	  }
+}
